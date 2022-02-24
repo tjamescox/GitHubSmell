536b4455d719ba035e7a6133f64c357488a85c42
Vipoup <60632610+Vipoup@users.noreply.github.com>
2022-02-23 20:01:04 -0500

    split function from puppeteer

--- MainSmell.js
+++ MainSmell.js
@@ -1,26 +1,36 @@
 function MainSmell(){
     // let input = document.querySelector('input');
     // let textarea = document.querySelector('textarea');
-    var input = document.getElementById('Input').value;
-    const puppeteer = require('puppeteer');
-
-    const http = require('http');
-    const hostname = '127.0.0.1';
-    const port = 3000;
-    const server = http.createServer((req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello World');});
+    document.getElementById("btn").addEventListener("click", ()=>{
+        document.getElementById("Output").value = "Loading..."});
     
-    server.listen(port, hostname, () => {
-        console.log(`Server running at http://${hostname}:${port}/`);});
-
-    (async () => {  const browser = await puppeteer.launch();  
-        const page = await browser.newPage();  
-        await page.goto(input);  
-        await page.screenshot({ path: 'mainPage.png' });
-        await browser.close();})();
-
-    document.getElementById('Output') = "hello";
-    document.getElementById("Input").addEventListener("click", displayInput);
+    let input = document.getElementById("Input").value;
+    document.getElementById("btn").addEventListener("click", ()=>{
+        document.getElementById("Output").value = document.getElementById("Output").value + '\n' + input});
 }
+
+const puppeteer = require('puppeteer');
+
+const http = require('http');
+const { Console } = require('console');
+const hostname = '127.0.0.1';
+const port = 3000;
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.write("CodeSmellerWeb.html");
+    res.end('');});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);});
+
+(async () => {  const browser = await puppeteer.launch();  
+    const page = await browser.newPage();  
+    await page.goto('https://github.com/Vipoup/Vulnerability-Scanner/blob/main/xss-attacks/server.js');  
+    thing = await page.evaluate(() => {
+        return document.querySelector('#LC').textContent;
+    });
+
+    console.log(thing);
+    //await page.screenshot({ path: 'mainPage.png' });
+    await browser.close();})();
