e0d3a7fd9e8429c7cca7038b0c8c54bcaed48367
mFall14 <91287153+mFall14@users.noreply.github.com>
2022-02-24 13:07:28 -0500

    commit with express implemented

--- MainSmell.js
+++ MainSmell.js
@@ -9,20 +9,35 @@ function MainSmell(){
         document.getElementById("Output").value = document.getElementById("Output").value + '\n' + input});
 }
 
+const express = require('express');
+const path = require('path');
 const puppeteer = require('puppeteer');
 
-const http = require('http');
-const { Console } = require('console');
-const hostname = '127.0.0.1';
-const port = 3000;
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.write("CodeSmellerWeb.html");
-    res.end('');});
+//Starting server with express
+const server = express();
+const port = process.env.PORT || 3000;
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);});
+// sendFile will go here
+server.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/CodeSmellerWeb.html'));
+  });
+
+server.listen(port);
+console.log('Server started at http://localhost:' + port);
+
+
+// const http = require('http');
+// const { Console } = require('console');
+// const hostname = '127.0.0.1';
+// const port = 3000;  
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.write("CodeSmellerWeb.html");
+//     res.end('');});
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);}
+// );
 
 (async () => {  const browser = await puppeteer.launch();  
     const page = await browser.newPage();  
@@ -33,4 +48,5 @@ server.listen(port, hostname, () => {
 
     console.log(thing);
     //await page.screenshot({ path: 'mainPage.png' });
-    await browser.close();})();
+    await browser.close();}
+)();
