8644cc45d2abd48d29ff199f54e4f92067fd7bac
Vipoup <60632610+Vipoup@users.noreply.github.com>
2022-02-28 20:20:51 -0500

    linked html to puppeteer

--- MainSmell.js
+++ MainSmell.js
@@ -20,7 +20,34 @@ const port = process.env.PORT || 3000;
 // sendFile will go here
 server.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/CodeSmellerWeb.html'));
-  });
+});
+
+
+server.get('/process_get', function (req, res) {
+    // Prepare output in JSON format
+    let response = {
+       GHLink:req.query.GHLink
+    };
+
+    //console.log(response.GHLink)
+
+    (async () => {  const browser = await puppeteer.launch();  
+        const page = await browser.newPage();  
+
+        //https://github.com/Vipoup/Vulnerability-Scanner/blob/main/xss-attacks/server.js
+        await page.goto(response.GHLink);  
+        thing = await page.evaluate(() => {
+
+            return document.querySelector('#LC1').textContent;
+        });
+    
+        console.log(thing);
+        //await page.screenshot({ path: 'mainPage.png' });
+        await browser.close();}
+    )();
+
+    res.end();
+ })
 
 server.listen(port);
 console.log('Server started at http://localhost:' + port);
@@ -39,14 +66,14 @@ console.log('Server started at http://localhost:' + port);
 //     console.log(`Server running at http://${hostname}:${port}/`);}
 // );
 
-(async () => {  const browser = await puppeteer.launch();  
-    const page = await browser.newPage();  
-    await page.goto('https://github.com/Vipoup/Vulnerability-Scanner/blob/main/xss-attacks/server.js');  
-    thing = await page.evaluate(() => {
-        return document.querySelector('#LC1').textContent;
-    });
+// (async () => {  const browser = await puppeteer.launch();  
+//     const page = await browser.newPage();  
+//     await page.goto('https://github.com/Vipoup/Vulnerability-Scanner/blob/main/xss-attacks/server.js');  
+//     thing = await page.evaluate(() => {
+//         return document.querySelector('#LC1').textContent;
+//     });
 
-    console.log(thing);
-    //await page.screenshot({ path: 'mainPage.png' });
-    await browser.close();}
-)();
+//     console.log(thing);
+//     //await page.screenshot({ path: 'mainPage.png' });
+//     await browser.close();}
+// )();
